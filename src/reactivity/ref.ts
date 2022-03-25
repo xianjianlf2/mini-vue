8fa4418699b70d6cf65bfc5771baa98d2e5f8d14
Mark <mark-xian@foxmail.com>
2022-03-26 01:33:08 +0800

    ✨ feat(实现 proxyRefs 功能):

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -56,3 +56,23 @@ export function isRef(ref) {
 export function unRef(ref) {
   return isRef(ref) ? ref.value : ref
 }
+
+export function proxyRefs(objectWithRefs) {
+  // get set
+  return new Proxy(objectWithRefs, {
+    get(target, key) {
+      // get => age(ref) 返回value
+      //  not ref => value
+      return unRef(Reflect.get(target, key))
+    },
+
+    set(target, key, value) {
+      //set =>ref .value
+      if (isRef(target[key]) && !isRef(value)) {
+        return (target[key].value = value)
+      } else {
+        return Reflect.set(target, key, value)
+      }
+    },
+  })
+}
