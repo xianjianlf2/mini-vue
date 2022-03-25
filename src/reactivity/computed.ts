3d4a8114db8e3b89bc0b364b65c882f15b49de84
Mark <mark-xian@foxmail.com>
2022-03-26 02:22:20 +0800

    ✨ feat(实现 computed 计算属性):

---
+++ src/reactivity/computed.ts
@@ -0,0 +1,32 @@
+import { ReactiveEffect } from './effect'
+class ComputedRefImpl {
+  private _getter: any
+  private _dirty: boolean = true
+  private _value: any
+  private _effect: ReactiveEffect
+  constructor(getter) {
+    this._getter = getter
+
+    this._effect = new ReactiveEffect(getter, () => {
+      if (!this._dirty) {
+        this._dirty = true
+      }
+    })
+  }
+
+  get value() {
+    // get
+    // 当依赖的响应式对象的值发生改变的时候
+    // effect
+    if (this._dirty) {
+      this._dirty = false
+      this._value = this._effect.run()
+    }
+
+    return this._value
+  }
+}
+
+export function computed(getter) {
+  return new ComputedRefImpl(getter)
+}
