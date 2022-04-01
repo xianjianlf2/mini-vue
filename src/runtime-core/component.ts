cdcf2e49cde008146b58dd5302101d4d3bcaad15
Mark <mark-xian@foxmail.com>
2022-04-02 01:11:31 +0800

    ✨ feat: 实现getCurrentInstance

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -31,11 +31,11 @@ function setupStatefulComponent(instance: any) {
   const { setup } = Component
 
   if (setup) {
-    // function Object
+    setCurrentInstance(instance)
     const setupResult = setup(shallowReadonly(instance.props), {
       emit: instance.emit,
     })
-
+    setCurrentInstance(null)
     handleSetupResult(instance, setupResult)
   }
 }
@@ -56,4 +56,13 @@ function finishComponentSetup(instance: any) {
   // if (Component.render) {
   instance.render = Component.render
   // }
+}
+
+let currentInstance = null
+export function getCurrentInstance() {
+  return currentInstance
+}
+
+export function setCurrentInstance(instance) {
+  currentInstance = instance
 }
