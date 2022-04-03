fa44b3c9ff720859fae2731327a93120b3ff76de
Mark <mark-xian@foxmail.com>
2022-04-03 16:34:58 +0800

    🐞 fix: createComp添加subTree

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -14,6 +14,7 @@ export function createComponentInstance(vnode, parent) {
     provides: parent ? parent.provides : {},
     parent,
     isMounted: false,
+    subTree: {},
     emit: () => {},
   }
   component.emit = emit.bind(null, component) as any
